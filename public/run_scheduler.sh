87de2447d8e9984909742e606c4f92dac23fb308
LemuelDev <lemueldev30@gmail.com>
2025-09-22 16:41:47 +0800

    change script

--- public/run_scheduler.sh
+++ public/run_scheduler.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
+#!/bin/sh 
 /usr/bin/php /home/u10282994/domains/epclinic.cloud/public_html/artisan schedule:run >> /home/u10282994/domains/epclinic.cloud/public_html/storage/logs/cron.log 2>&1
