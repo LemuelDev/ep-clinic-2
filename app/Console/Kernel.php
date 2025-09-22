c94487dcaa046796cb6d56672b3f860f2e1562a4
LemuelDev <lemueldev30@gmail.com>
2025-09-22 17:47:01 +0800

    change scheduler to every 8am in the morning

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,14 +14,14 @@ class Kernel extends ConsoleKernel
     {
         //  --- FOR LOCAL TESTING ONLY ---
         // This will make your reminder command eligible to run every minute
-        $schedule->command('appointments:send-reminders')
-                 ->everyMinute() // <--- CHANGE THIS LINE FOR TESTING
-                 ->timezone('Asia/Manila');
+        // $schedule->command('appointments:send-reminders')
+        //          ->everyMinute() // <--- CHANGE THIS LINE FOR TESTING
+        //          ->timezone('Asia/Manila');
 
         // // --- FOR PRODUCTION, IT SHOULD BE LIKE THIS: ---
-        // $schedule->command('appointments:send-reminders')
-        //          ->dailyAt('08:00')
-        //          ->timezone('Asia/Manila');
+        $schedule->command('appointments:send-reminders')
+                 ->dailyAt('08:00')
+                 ->timezone('Asia/Manila');
     }
 
     /**
