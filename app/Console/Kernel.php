d34e40b5df364686811f53029d65b6ceaae15685
LemuelDev <lemueldev30@gmail.com>
2025-09-25 13:29:46 +0800

    change scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     {
         // // --- FOR PRODUCTION, IT SHOULD BE LIKE THIS: ---
         $schedule->command('appointments:send-reminders')
-                 ->dailyAt('00:00')
+                 ->dailyAt('08:00')
                  ->timezone('Asia/Manila');
     }
 
